88c47db8abe21ec3af1d69087719578d302510c8
Krishna Tandon <tandonkrishna1208@gmail.com>
2022-11-08 18:02:12 +0530

    Dependencies config.

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+//Initialize the app by equating to express();
+
+const app = express();
