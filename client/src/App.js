57abc60091aac19cc898a9d6024e68dbaa9086c3
Krishna Tandon <tandonkrishna1208@gmail.com>
2022-11-09 13:25:38 +0530

    express setup configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+const arr = [1, 2, 3];
 
 const App = () => {
     return (
         <div>
             <h1>App</h1>
+            {arr.map(item => <p>{item}</p>)}
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,28 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-//Initialize the app by equating to express();
+import postRoutes from "./routes/posts.js";
 
+//Initialize the app by equating to express();
 const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//Using the express middleware (app.use() method) to connect the routes with our app. 
+app.use("/posts", postRoutes);
+// `/posts` means whenever we visit anything inside postRoutes, 
+// the url will have /posts appended.
+// Example : www.memories.com/posts/<your other called methods>
+
+//Setting up connection with MongoDB.
+const CONNECTION_URL = "mongodb+srv://krishna:1kT1UP63qP2w7ix6@cluster0.hzkzn.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//Deprecated
+//mongoose.set('useFindAndModify', false);
